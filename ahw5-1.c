53e8946ead3d4112dbb4e241e83bbb291da2ee77
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-11-07 22:48:52 +0900

    ahw5-1.c

--- ahw5-1.c
+++ ahw5-1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int m, n, steps ;
+	steps = 0;
+	scanf("%d", &n);
+	scanf("%d", &m);
+	while (n < m) {
+		if (n * 2 <= m) {
+		n = n * 2;
+		} else {
+		n = n + 1;
+		}
+		steps = steps + 1;
+		}
+	printf("Numbers of steps taken: %d", steps);
+	if (n > m) {
+	printf("Cannot transform %n to %m", n, m);
+	}
+return 0;
+}
+
+
+
+
+
 
