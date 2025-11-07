2d80a7bf778fd9a931fc156088eb4918c0725e5b
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-11-07 21:27:06 +0900

    hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n ;
+	 printf("Please enter two different numbers");
+	scanf("%d", &i);
+	scanf("%d", &n);
+                while ( n == i){
+		printf ("Same, pleaase enter two different numbers\n");
+	scanf("%d", &i);
+	scanf("%d", &n);
+	}
+	if (n != i) {
+		printf("You entered %d %d\n", i, n);
+}
+	return 0;
+}
+
 
